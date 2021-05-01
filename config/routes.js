9fe3b3ea2e85c5fcbbe2d50477aa8794a35c7a81
fermarinsanchez <fermarinsanchez@yahoo.es>
2021-05-01 23:43:49 +0200

    fix: remove spaces on routes.js

--- config/routes.js
+++ config/routes.js
@@ -3,14 +3,10 @@ const router = express.Router();
 const detailsController = require("../controllers/details.controller");
 
 
-router.post("/details", detailsController.create);
-
 router.get("/details", detailsController.readAllDetails);
-
 router.get("/details/:id", detailsController.readOne);
-
+router.post("/details", detailsController.create);
 router.patch("/details/:id", detailsController.update);
-
 router.delete("/details/:id", detailsController.delete);
 
 module.exports = router;
